cc9fbffd9b308fe4f04eb990c8b72428bd3e1528
TheNanoBay <noorani.mehdi@outlook.com>
2021-02-27 19:00:53 -0600

    commit tree 48e12c65dfc52f61ca19f27c9b92577ea2eb9f65

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,32 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "",
+      remainingCharacters: 0
+    };
+  }
+
+  handleChange = (event) =>{
+
+    let currentCharacters = event.target.value.length
+    let remainingCharacters = 280 - currentCharacters
+
+
+    this.setState({
+      message:event.target.value,
+      remainingCharacters: remainingCharacters
+    })
+
+    //debugger
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.handleChange} value={this.state.message} />
+        <h5>Characters left: {this.state.remainingCharacters}</h5>
       </div>
     );
   }
